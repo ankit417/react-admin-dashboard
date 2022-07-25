e167f6331898240620bd7264c859d1ae3db9e9f7
Ankit Acharya <acharya.ankit417@gmail.com>
2022-07-25 08:55:53 +0545

    file uploader component

---
+++ src/components/fileUploader/fileUploader.component.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const FileUploader = () => {
+  return (
+    <div className="file-uploader-wrapper">
+      <div>File Uploader Component</div>
+    </div>
+  );
+};
