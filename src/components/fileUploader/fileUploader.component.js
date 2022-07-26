9a32564c03727a1d2af1d6e4597b2a768e582ec1
Ankit Acharya <acharya.ankit417@gmail.com>
2022-07-26 07:31:30 +0545

    upload and remove files

--- src/components/fileUploader/fileUploader.component.js
+++ src/components/fileUploader/fileUploader.component.js
@@ -17,52 +17,54 @@ export const FileUploader = () => {
     setUserFiles(files);
   };
   return (
-    <div className="file-uploader-wrapper">
-      <div className="file-uploader-wrapper-container">
-        <div className="file-uploader-wrapper-container-header">
-          Upload Files
-        </div>
-        <div className="image-dropzone-section">
-          <div className="dropzone-default-image">
-            <img src={BG_IMAGE} alt="upload_image_placeholder" />
+    <div style={{ width: 800 }}>
+      <div className="file-uploader-wrapper">
+        <div className="file-uploader-wrapper-container">
+          <div className="file-uploader-wrapper-container-header">
+            Upload Files
           </div>
-        </div>
-        <div className="file-viewer">
-          {userFiles.length > 0 &&
-            userFiles.map((item, index) => {
-              return (
-                <div className="uploaded-file-item" key={index}>
-                  <img src={URL.createObjectURL(item)} alt="files upload" />
-                  <div className="uploaded-item-name">{item.name} </div>
-                  <div
-                    className="uploaded-item-remove"
-                    onClick={() => {
-                      removeFile(index);
-                    }}
-                  >
-                    {" "}
-                    X{" "}
+          <div className="image-dropzone-section">
+            <div className="dropzone-default-image">
+              <img src={BG_IMAGE} alt="upload_image_placeholder" />
+            </div>
+          </div>
+          <div className="file-viewer">
+            {userFiles.length > 0 &&
+              userFiles.map((item, index) => {
+                return (
+                  <div className="uploaded-file-item" key={index}>
+                    <img src={URL.createObjectURL(item)} alt="files upload" />
+                    <div className="uploaded-item-name">{item.name} </div>
+                    <div
+                      className="uploaded-item-remove"
+                      onClick={() => {
+                        removeFile(index);
+                      }}
+                    >
+                      {" "}
+                      X{" "}
+                    </div>
                   </div>
-                </div>
-              );
-            })}
-        </div>
-        <div className="file-uploader-upload">
-          <input
-            type="file"
-            onChange={(file) => {
-              getFiles(file);
-            }}
-            multiple={true}
-            id="file-uploader-input"
-          />
-          <label
-            className="abstract-file-upload-button"
-            htmlFor="file-uploader-input"
-          >
-            Choose file
-          </label>
-          <button>upload file</button>
+                );
+              })}
+          </div>
+          <div className="file-uploader-upload">
+            <input
+              type="file"
+              onChange={(file) => {
+                getFiles(file);
+              }}
+              multiple={true}
+              id="file-uploader-input"
+            />
+            <label
+              className="abstract-file-upload-button"
+              htmlFor="file-uploader-input"
+            >
+              Choose file
+            </label>
+            <button>upload file</button>
+          </div>
         </div>
       </div>
     </div>
